6de27ba5e616fa69373a280573eed45f825399e6
rbamarc <rbamarc929@gmail.com>
2023-02-01 06:34:57 -0800

    adding more inputs from user with error

--- src/main/java/netbeans/myscanner/MyScanner.java
+++ src/main/java/netbeans/myscanner/MyScanner.java
@@ -11,8 +11,8 @@ public class MyScanner {
         //declare the number variable and initialize to 0
         String name = "";
         int age = 0;
-        //int computers = 0;
-        //String hometown = "";
+        int computers = 0;
+        String hometown = "";
         
         //declare and initialize a scanner object
         Scanner myScanner = new Scanner(System.in);
@@ -27,8 +27,21 @@ public class MyScanner {
         //now wait for user to enter their name
         age = myScanner.nextInt();
         
-        //we will get the other values in the next commit
+        //ask user to enter the number of computers
+        //they have
+        System.out.println("Please enter the number of computers you own: ");
+        //now wait until the user types the number of computers
+        computers = myScanner.nextInt();
         
-        System.out.println("Hi " + name + " your age is: " + age);
+        //ask user to input their hometown
+        System.out.println("Please enter your hometown: ");
+        //now wait for hometown to be input
+        hometown = myScanner.nextLine();
+        
+        //now print the information to the console
+        
+        System.out.println("Hi " + name + " from " + hometown);
+        System.out.println("Your age is: " + age);
+        System.out.println("Number of computers you own: " + computers);
     }
 }
